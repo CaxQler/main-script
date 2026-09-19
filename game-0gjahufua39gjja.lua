c1080d15dac4da15bca42100c8e54ed970250ea2
nigas-qler <sdd106485@gmail.com>
2026-09-19 18:24:02 +0700

    Update game-0gjahufua39gjja.lua

--- game-0gjahufua39gjja.lua
+++ game-0gjahufua39gjja.lua
@@ -1,7 +1,7 @@
 local placeId = tostring(game.PlaceId)
 
 local gameScripts = {
-    ["124216119978534"]      = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/CaxQler/scripts-main/refs/heads/main/ride-a-pets.lua"))()',
+    ["124216119978534"]      = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/CaxQler/scripts-main/refs/heads/main/ride.lua"))()',
 }
 
 local scriptToRun = gameScripts[placeId]
